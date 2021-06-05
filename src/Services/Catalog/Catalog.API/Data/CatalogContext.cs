f31e0502ea1d1e24fef806ba785dcaf3e08c6689
Niraj Patel <nirajp82@yahoo.com>
2021-06-05 19:51:42 -0400

    Add Catalog Repository

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly IConfiguration _configuration;
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var client = new MongoClient(GetConfigValue("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(GetConfigValue("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(GetConfigValue("DatabaseSettings:CollectionName"));
+        }
+
+
+        private string GetConfigValue(string key) => _configuration.GetValue<string>(key);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,58 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _catalogContext;
+
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            _catalogContext = catalogContext ?? throw new ArgumentNullException(nameof(catalogContext));
+        }
+
+        public async Task CreateProductAsync(Product product)
+        {
+            await _catalogContext.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProductAsync(string id)
+        {
+            FilterDefinition<Product> productFilter = Builders<Product>.Filter.Eq(prop => prop.Id, id);
+            DeleteResult deleteResult = await _catalogContext.Products.DeleteOneAsync(productFilter);
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<Product> GetProductAsync(string id)
+        {
+            return await _catalogContext.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategoryAsync(string categoryName)
+        {
+            return await _catalogContext.Products.Find(p => p.Category == categoryName).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByNameAsync(string name)
+        {
+            return await _catalogContext.Products.Find(p => p.Name == name).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _catalogContext.Products.Find(p => true).ToListAsync();
+        }
+
+        public async Task<bool> UpdateProductAsync(Product product)
+        {
+            var updateResult = await _catalogContext.Products.ReplaceOneAsync(filter: p => p.Id == product.Id, replacement: product);
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+    }
+}
